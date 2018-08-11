495c976f1a5df834f9a1080a90f2ee21acd05d66
sa2taka <sa2taka@gmail.com>
2018-08-11 12:01:20 +0900

    修正: #7

--- src/main/index.js
+++ src/main/index.js
@@ -54,42 +54,8 @@ function createWindow () {
   mainWindow.on('closed', () => {
     mainWindow = null
   })
-
-  mainWindow.on('resize', () => {
-    let bounds = mainWindow.getBounds()
-    if (bounds.width < 500) {
-      bounds.width = 500
-      mainWindow.setBounds(bounds)
-    }
-  })
-
-  createMenu()
 }
 
-function createMenu () {
-  // Create the Application's main menu
-  var template = [{
-    label: 'Application',
-    submenu: [
-      { label: 'Quit', accelerator: 'Command+Q', click: function () { app.quit() } }
-    ]
-  },
-  {
-    label: 'Edit',
-    submenu: [
-      { label: 'Undo', accelerator: 'CmdOrCtrl+Z', selector: 'undo:' },
-      { label: 'Redo', accelerator: 'Shift+CmdOrCtrl+Z', selector: 'redo:' },
-      { type: 'separator' },
-      { label: 'Cut', accelerator: 'CmdOrCtrl+X', selector: 'cut:' },
-      { label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:' },
-      { label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:' },
-      { label: 'Select All', accelerator: 'CmdOrCtrl+A', selector: 'selectAll:' }
-    ]
-  }
-  ]
-
-  Menu.setApplicationMenu(Menu.buildFromTemplate(template))
-}
 app.on('ready', createWindow)
 
 app.on('window-all-closed', () => {
